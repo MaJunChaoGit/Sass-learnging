a9e92d4cd22da2f9b747f113537b4289e9f0b2d6
majunchao <354488954@qq.com>
2018-07-06 22:49:46 +0800

    Sass基础学习看了一遍
    Sass基础学习看了一遍

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,31 +18,33 @@
         父选择器标识符还有另外一种用法，你可以在父选择器之前添加选择器
         <a href="">内嵌在群组选择器内的嵌 套规则</a>
       </aside>
-      <article>
+      <article class="seriousError">
         我是文章外包围
         <section>子组合选择器和同层组合选择器</section>
         <dl>
           <dt>子组合选择器和同层组合选择器</dt>
           <dd>子组合选择器和同层组合选择器</dd>
         </dl>
-        <h1>我是H1标题</h1>
+        <h1 class="seriousError">我是H1标题</h1>
         <p>我是文章</p>
         <a href="">父选择器的标识符&;</a>
       </article>
       <article>子组合选择器和同层组合选择器</article>
       <article>子组合选择器和同层组合选择器</article>
     </div>
-    <div class="fancybox"></div>
-    <div class="blue-theme">
+    <div class="fancybox seriousError"></div>
+    <div class="blue-theme seriousError">
       <aside>
         我是嵌套导入的蓝色主题
       </aside>
     </div>
-    <div class="notice">我是圆角混合器</div>
-    <ul class="plain">
+    <div class="notice seriousError">我是圆角混合器</div>
+    <ul class="plain seriousError">
       <li>我是@mixin混合器的css规则1</li>
       <li>我是@mixin混合器的css规则2</li>
       <li>我是@mixin混合器的css规则3</li>
+      <a href="" class="disabled">我是继承a标签的</a>
+      <a href="">我是继承a标签的</a>
     </ul>
   </div>
 </template>
@@ -182,11 +184,60 @@ ul.plain {
 }
 // 给混合器传参数
 @mixin link-colors($normal,$hover,$visited){
-  color:$    ;
+  color:$normal;
   &:hover{color:$hover;}
   &:visited{color:$visited;}
 }
-a{
-  @include link-colors(green,red,blue);
+// a{
+//   @include link-colors(black,red,blue);
+// }
+// 当你@include混合器时，有时候可能会很难区分每个参数是什么意思，参数之间是一个什么样的顺序。为了解决这个问题，sass允许通过语法$name: value的形式指定每个参数的值。这种形式的传参，参数顺序就不必再在乎了，只需要保证没有漏掉参数即可：
+// a{
+//   @include link-colors(
+//     $hover:green,
+//     $normal:red,
+//     $visited:blue
+//   )
+// }
+ // 默认参数值;
+ @mixin link-colors(
+  $normal,
+  $hover:$normal,
+  $visited:$normal
+)
+{
+  color:$normal;
+  &:hover{
+    color:$hover;
+  }
+  &:visited{
+    color:$visited;
+  }
 }
+// a{
+//   @include link-colors(red);
+// }
+// 使用选择器继承来精简CSS
+.error{
+  border:1px solid red;
+  background-color: #fdd;
+}
+.seriousError{
+  @extend .error;
+  border-width:3px;
+}
+// .seriousError不仅会继承.error自身的所有样式，任何跟.error有关的组合选择器样式也会被.seriousError以组合选择器的形式继承，如下代码:
+.error a {
+  // color: red;
+  font-weight:500;
+}
+h1.error{
+  font-size:1.2rem;
+}
+// 大多数情况你可能只想对类使用继承,但是有些场合你可能想做的更多.最常用的一种高级用法是继承html样式
+.disabled{
+  color:gray;
+  @extend a;
+}
+// 使用继承的最佳实践
 </style>
